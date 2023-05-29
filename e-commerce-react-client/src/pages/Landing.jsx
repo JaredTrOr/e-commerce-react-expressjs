b309b52e56bc60f234d4dc27444c4209060550d7
JaredTrOr <trujillojaredalexander@gmail.com>
2023-05-28 22:03:41 -0600

    Addition of basic navar css and landing page

--- e-commerce-react-client/src/pages/Landing.jsx
+++ e-commerce-react-client/src/pages/Landing.jsx
@@ -1,13 +1,24 @@
+import landingImage from '../images/landing/landing-image.png'
+import '../styles/landing.css'
+
 function Landing() {
     return(
         <div className="main-container">
-            <div className="heading-section">
+            <div className="heading-section mt-4">
                 <div className="heading-section-title">
-                    <h1>Welcome to E-commerce JS, the first website of second hand clothes</h1>
+                    <div>
+                        <h1>
+                            Welcome to E-commerce JS, the first website of second handed clothes.
+                        </h1>
+                        <div className='heading-section-buttons mt-2'>
+                            <button className='squared-button create-account-button bold'>Create account</button>
+                            <button className='squared-button login-button bold'>Log-in</button>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="heading-section-ilustration">
-                
+                    <img width={700} src={landingImage} alt="" />
                 </div>
             </div>
 
